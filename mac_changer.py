9ca06bfecc9d226735ada34aba4b21388d9d46fa
Vagish Mohanakumar <iamvagish6@gmail.com>
2023-03-21 14:08:49 +0530

    adding modified file to the repository

--- mac_changer.py
+++ mac_changer.py
@@ -1,8 +1,13 @@
 import subprocess
+import socket
 
-print("Welcome User...")
-command = input('Enter MAC Address: ')
-network_interface = input("Enter Network Interface to Change: ")
+user = input("User Name > ")
+print(f"Welcome {format(user)}...")
+
+command = input('Enter MAC Address > ')
+network_interface = input("Enter Network Interface to change > ")
+
+interface_list = ["eth0", "wlan0"]
 
 
 class Change_Mac():
@@ -11,15 +16,18 @@ class Change_Mac():
 
     def change_mac(self, mac_input, network_interface_input):
         if len(mac_input) < 17:
-            print("[-] entered character is less than 17")
+            print("[-] entered characters are less than 17")
         elif len(mac_input) > 17:
-            print("[-] entered character is greater than 17")
+            print("[-] entered characters are greater than 17")
+        elif network_interface_input == "":
+            print("[-] Network Interface Device has not been provided")
+        elif network_interface_input not in interface_list:
+            print("[-] Provided interface is Invalid")
         else:
             subprocess.call(f"ifconfig {format(network_interface_input)} down", shell=True)
             subprocess.call(f"ifconfig {format(network_interface_input)} hw ether {format(mac_input)}", shell=True)
             subprocess.call(f"ifconfig {format(network_interface_input)} up", shell=True)
             print(f'[+] MAC Address Changed to {format(mac_input)}, for network interface: ' + network_interface_input)
-
 
 class Main():
     run_command = Change_Mac()
